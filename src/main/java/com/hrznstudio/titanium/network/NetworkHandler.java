f52116fcbc4275d7c660c6a9210abe8452847de4
TheCodedOne <dr.kevincodex@gmail.com>
2018-07-01 22:13:57 +0100

    Add network system

---
+++ src/main/java/com/hrznstudio/titanium/network/NetworkHandler.java
@@ -0,0 +1,21 @@
+/*
+ * This file is part of Titanium
+ * Copyright (C) 2018, Horizon Studio <contact@hrznstudio.com>, All rights reserved.
+ */
+package com.hrznstudio.titanium.network;
+
+import com.hrznstudio.titanium.Titanium;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.relauncher.Side;
+
+public class NetworkHandler {
+    public static SimpleNetworkWrapper NETWORK = new SimpleNetworkWrapper(Titanium.MODID);
+    private static int i = 0;
+
+    public static <REQ extends IMessage, REPLY extends IMessage> void registerMessage(Class<? extends IMessageHandler<REQ, REPLY>> handler, Class<REQ> message, Side side) {
+        NETWORK.registerMessage(handler, message, i++, side);
+    }
+
+}
