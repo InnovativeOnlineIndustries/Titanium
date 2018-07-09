b132f46b67841a79ceadb4b585e13402f122db70
TheCodedOne <dr.kevincodex@gmail.com>
2018-07-10 00:10:06 +0100

    Prevent multiple static assets being created

--- src/main/java/com/hrznstudio/titanium/client/gui/DefaultAssetProvider.java
+++ src/main/java/com/hrznstudio/titanium/client/gui/DefaultAssetProvider.java
@@ -10,35 +10,42 @@ import net.minecraft.util.ResourceLocation;
 import javax.annotation.Nonnull;
 import java.awt.*;
 
-public class DefaultAssetProvider implements IAssetProvider {
+public final class DefaultAssetProvider implements IAssetProvider {
+    DefaultAssetProvider() {
+    }
+
+    private final IAsset SLOT = new IAsset() {
+        @Override
+        public ResourceLocation getResourceLocation() {
+            return DEFAULT_LOCATION;
+        }
+
+        @Override
+        public Rectangle getArea() {
+            return new Rectangle(1, 185, 18, 18);
+        }
+    };
+
+    private final IAsset BACKGROUND = new IAsset() {
+        @Override
+        public ResourceLocation getResourceLocation() {
+            return DEFAULT_LOCATION;
+        }
+
+        @Override
+        public Rectangle getArea() {
+            return new Rectangle(0, 0, 176, 184);
+        }
+    };
+
     @Nonnull
     @Override
     public IAsset getAsset(AssetType assetType) {
         switch (assetType) {
             case BACKGROUND:
-                return new IAsset() {
-                    @Override
-                    public ResourceLocation getResourceLocation() {
-                        return DEFAULT_LOCATION;
-                    }
-
-                    @Override
-                    public Rectangle getArea() {
-                        return new Rectangle(0, 0, 176, 184);
-                    }
-                };
+                return BACKGROUND;
             case SLOT:
-                return new IAsset() {
-                    @Override
-                    public ResourceLocation getResourceLocation() {
-                        return DEFAULT_LOCATION;
-                    }
-
-                    @Override
-                    public Rectangle getArea() {
-                        return new Rectangle(1, 185, 18, 18);
-                    }
-                };
+                return SLOT;
         }
         throw new RuntimeException("An error has occurred, default provider encountered an unknown asset type");
     }
