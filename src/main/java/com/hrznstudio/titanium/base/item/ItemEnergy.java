5179a5ccf261435e0ccc47fede104c8c2cb60abc
TheCodedOne <coded@hrznstudio.com>
2018-08-24 22:24:10 +0100

    i am angry

--- src/main/java/com/hrznstudio/titanium/base/item/ItemEnergy.java
+++ src/main/java/com/hrznstudio/titanium/base/item/ItemEnergy.java
@@ -1,6 +1,8 @@
 /*
  * This file is part of Titanium
  * Copyright (C) 2018, Horizon Studio <contact@hrznstudio.com>, All rights reserved.
+ *
+ * This means no, you cannot steal this code. This is licensed for sole use by Horizon Studio and its subsidiaries, you MUST be granted specific written permission by Horizon Studio to use this code, thinking you have permission IS NOT PERMISSION!
  */
 package com.hrznstudio.titanium.base.item;
 
