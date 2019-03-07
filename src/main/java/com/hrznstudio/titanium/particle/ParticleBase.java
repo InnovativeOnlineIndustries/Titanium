84043bbe8e818b60f8bfc80a54ecbe459c75991b
EpicSquid <epicsquid315@gmail.com>
2019-03-07 22:51:00 +1100

    Finally freaking fixed the license

--- src/main/java/com/hrznstudio/titanium/particle/ParticleBase.java
+++ src/main/java/com/hrznstudio/titanium/particle/ParticleBase.java
@@ -4,6 +4,7 @@
  *
  * This means no, you cannot steal this code. This is licensed for sole use by Horizon Studio and its subsidiaries, you MUST be granted specific written permission by Horizon Studio to use this code, thinking you have permission IS NOT PERMISSION!
  */
+
 package com.hrznstudio.titanium.particle;
 
 import net.minecraft.client.Minecraft;
