bb6e3baf5b753e4fe9f4c227f72da86ac489ae22
Coded <coded@hrznstudio.com>
2019-04-29 18:46:34 +0100

    Update license header

--- src/main/java/com/hrznstudio/titanium/particle/ParticleRenderer.java
+++ src/main/java/com/hrznstudio/titanium/particle/ParticleRenderer.java
@@ -2,7 +2,7 @@
  * This file is part of Titanium
  * Copyright (C) 2019, Horizon Studio <contact@hrznstudio.com>, All rights reserved.
  *
- * This means no, you cannot steal this code. This is licensed for sole use by Horizon Studio and its subsidiaries, you MUST be granted specific written permission by Horizon Studio to use this code, thinking you have permission IS NOT PERMISSION!
+ * This means no, you cannot use this code. This is licensed for sole use by Horizon and it's partners, you MUST be granted specific written permission from Horizon to use this code.
  */
 
 package com.hrznstudio.titanium.particle;
