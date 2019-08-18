354677dd45b22a04d16ae6539f56d132edcb6eb6
Buuz135 <buuz135@hotmail.com>
2019-08-18 23:25:20 +0200

    Updated Licenses

--- src/main/java/com/hrznstudio/titanium/recipe/serializer/JSONSerializableDataHandler.java
+++ src/main/java/com/hrznstudio/titanium/recipe/serializer/JSONSerializableDataHandler.java
@@ -1,3 +1,10 @@
+/*
+ * This file is part of Titanium
+ * Copyright (C) 2019, Horizon Studio <contact@hrznstudio.com>.
+ *
+ * This code is licensed under GNU Lesser General Public License v3.0, the full license text can be found in LICENSE.txt
+ */
+
 package com.hrznstudio.titanium.recipe.serializer;
 
 
@@ -56,6 +63,7 @@ public class JSONSerializableDataHandler {
         int mods = f.getModifiers();
         return !Modifier.isFinal(mods) && !Modifier.isStatic(mods) && !Modifier.isTransient(mods) && FIELD_SERIALIZER.containsKey(type);
     }
+
     public static <T> T read(Class<T> type, JsonElement element) {
         return (T) FIELD_SERIALIZER.get(type).getSecond().read(element);
     }
