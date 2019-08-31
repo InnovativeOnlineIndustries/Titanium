feddf5966d8f2e528655f98c0b8d3efbed535070
Buuz135 <buuz135@hotmail.com>
2019-08-31 20:46:31 +0200

    Added fluid support

---
+++ src/main/java/com/hrznstudio/titanium/fluid/TitaniumFluidInstance.java
@@ -0,0 +1,58 @@
+package com.hrznstudio.titanium.fluid;
+
+import com.hrznstudio.titanium.module.api.IAlternativeEntries;
+import com.hrznstudio.titanium.module.api.RegistryManager;
+import net.minecraft.block.Block;
+import net.minecraft.block.FlowingFluidBlock;
+import net.minecraft.block.material.Material;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.item.BucketItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Items;
+import net.minecraftforge.fluids.FluidAttributes;
+
+public class TitaniumFluidInstance extends net.minecraftforge.registries.ForgeRegistryEntry<TitaniumFluidInstance> implements IAlternativeEntries {
+
+    private TitaniumFluid flowingFluid;
+    private TitaniumFluid sourceFluid;
+    private Item bucketFluid;
+    private Block blockFluid;
+
+    public TitaniumFluidInstance(String modid, String fluid, FluidAttributes attributes, boolean hasBucket, ItemGroup group) {
+        this.sourceFluid = (TitaniumFluid) new TitaniumFluid.Source(attributes).setRegistryName(modid, fluid);
+        this.flowingFluid = (TitaniumFluid) new TitaniumFluid.Flowing(attributes).setRegistryName(modid, fluid + "_fluid");
+        this.sourceFluid.setSourceFluid(sourceFluid).setFlowingFluid(flowingFluid);
+        this.flowingFluid.setSourceFluid(sourceFluid).setFlowingFluid(flowingFluid);
+        if (hasBucket)
+            this.bucketFluid = new BucketItem(this.sourceFluid, new Item.Properties().containerItem(Items.BUCKET).maxStackSize(1).group(group)).setRegistryName(modid, fluid + "_bucket");
+        this.blockFluid = new FlowingFluidBlock(sourceFluid, Block.Properties.create(Material.WATER).doesNotBlockMovement().hardnessAndResistance(100.0F).noDrops()) {
+        }.setRegistryName(modid, fluid + "_block");
+        this.sourceFluid.setBlockFluid(blockFluid).setBucketFluid(bucketFluid);
+        this.flowingFluid.setBlockFluid(blockFluid).setBucketFluid(bucketFluid);
+    }
+
+    @Override
+    public void addAlternatives(RegistryManager registry) {
+        registry.content(Fluid.class, flowingFluid);
+        registry.content(Fluid.class, bucketFluid);
+        registry.content(Block.class, blockFluid);
+        if (bucketFluid != null) registry.content(Item.class, bucketFluid);
+    }
+
+    public TitaniumFluid getFlowingFluid() {
+        return flowingFluid;
+    }
+
+    public TitaniumFluid getSourceFluid() {
+        return sourceFluid;
+    }
+
+    public Item getBucketFluid() {
+        return bucketFluid;
+    }
+
+    public Block getBlockFluid() {
+        return blockFluid;
+    }
+}
