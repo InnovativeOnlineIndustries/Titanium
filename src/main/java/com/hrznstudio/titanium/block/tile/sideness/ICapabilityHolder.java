1ecfa6b48a9c8d85c4c906d9c3850161a0ad6b43
Buuz135 <buuz135@hotmail.com>
2019-09-10 16:54:08 +0200

    Removed unused method

--- src/main/java/com/hrznstudio/titanium/block/tile/sideness/ICapabilityHolder.java
+++ src/main/java/com/hrznstudio/titanium/block/tile/sideness/ICapabilityHolder.java
@@ -11,13 +11,10 @@ import com.hrznstudio.titanium.util.FacingUtil;
 import net.minecraftforge.common.util.LazyOptional;
 
 import javax.annotation.Nullable;
-import java.util.HashMap;
 
 public interface ICapabilityHolder<T extends Object, R extends Object> {
 
     void add(T capability);
-
-    HashMap<FacingUtil.Sideness, LazyOptional<R>> getCapabilities();
 
     LazyOptional<R> getCapabilityForSide(@Nullable FacingUtil.Sideness sideness);
 
