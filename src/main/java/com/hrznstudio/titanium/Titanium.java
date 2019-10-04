7c9aa24957e92b3730312db8ee530e131a5053dd
Buuz135 <buuz135@hotmail.com>
2019-10-04 17:51:15 +0200

    Moved resource overwriting and scanning to preInit

--- src/main/java/com/hrznstudio/titanium/Titanium.java
+++ src/main/java/com/hrznstudio/titanium/Titanium.java
@@ -97,13 +97,17 @@ public class Titanium extends ModuleController {
     }
 
     @Override
-    protected void initModules() {
+    public void onPreInit() {
+        super.onPreInit();
         ResourceRegistry.scanForReferences();
         ResourceRegistry.getOrCreate("iron").addOverride(ResourceType.INGOT, Items.IRON_INGOT).addOverride(ResourceType.NUGGET, Items.IRON_NUGGET);
         ResourceRegistry.getOrCreate("iron").setColor(0xd8d8d8).addAll(ResourceType.values());
         ResourceRegistry.getOrCreate("gold").addOverride(ResourceType.INGOT, Items.GOLD_INGOT).addOverride(ResourceType.NUGGET, Items.GOLD_NUGGET);
         ResourceRegistry.getOrCreate("gold").setColor(0xfad64a).addAll(ResourceType.values());
+    }
 
+    @Override
+    protected void initModules() {
         addModule(Module.builder("core").force()
                 .feature(Feature.builder("core").force()
                         .content(ContainerType.class, (ContainerType) IForgeContainerType.create(ContainerTileBase::new).setRegistryName(new ResourceLocation(Titanium.MODID, "tile_container")))));
