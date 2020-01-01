7fa358d29c16f85343aaaf76f0592e1ea7e4b551
Buuz135 <buuz135@hotmail.com>
2020-01-01 17:29:07 +0100

    Ready for build

--- src/main/java/com/hrznstudio/titanium/TitaniumClient.java
+++ src/main/java/com/hrznstudio/titanium/TitaniumClient.java
@@ -11,9 +11,11 @@ import com.hrznstudio.titanium.block.BlockBase;
 import com.hrznstudio.titanium.util.RayTraceUtils;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ActiveRenderInfo;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.PlayerRenderer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
@@ -32,14 +34,14 @@ public class TitaniumClient {
     }
 
     @OnlyIn(Dist.CLIENT)
-    public static void blockOverlayEvent(DrawHighlightEvent event) {
-        if (event.getTarget() instanceof BlockRayTraceResult) {
-            BlockRayTraceResult traceResult = (BlockRayTraceResult) event.getTarget();
+    public static void blockOverlayEvent(DrawHighlightEvent.HighlightBlock event) {
+        if (event.getTarget() != null) {
+            BlockRayTraceResult traceResult = event.getTarget();
             BlockState og = Minecraft.getInstance().world.getBlockState(traceResult.getPos());
             if (og.getBlock() instanceof BlockBase && ((BlockBase) og.getBlock()).hasIndividualRenderVoxelShape()) {
                 VoxelShape shape = RayTraceUtils.rayTraceVoxelShape(traceResult, Minecraft.getInstance().world, Minecraft.getInstance().player, 32, event.getPartialTicks());
                 if (shape != null) {
-                    event.setCanceled(true);
+                    //event.setCanceled(true);
                     RenderSystem.enableBlend();
                     RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
                     RenderSystem.lineWidth(Math.max(2.5F, (float) Minecraft.getInstance().func_228018_at_().getFramebufferWidth() / 1920.0F * 2.5F)); //MainWindow
@@ -53,7 +55,14 @@ public class TitaniumClient {
                     double d0 = info.getProjectedView().x;
                     double d1 = info.getProjectedView().y;
                     double d2 = info.getProjectedView().z;
+                    //MatrixStack stack = new MatrixStack();
+                    //stack.func_227860_a_();
+                    //stack.func_227861_a_(0,0,0);
+                    //stack.func_227861_a_((double) blockpos.getX() - d0, (double) blockpos.getY() - d1, (double) blockpos.getZ() - d2);
+                    IVertexBuilder buffer = Minecraft.getInstance().func_228019_au_().func_228490_d_().getBuffer(RenderType.func_228659_m_());
+                    //WorldRenderer.func_228431_a_(stack, buffer, shape,(double) blockpos.getX(), (double) blockpos.getY() , (double) blockpos.getZ(), 1.0F, 1.0F, 1.0F, 1.0F);
                     //TODO Minecraft.getInstance().worldRenderer.drawShape(shape, (double) blockpos.getX() - d0, (double) blockpos.getY() - d1, (double) blockpos.getZ() - d2, 0.0F, 0.0F, 0.0F, 0.4F);
+                    //stack.func_227865_b_();
                     RenderSystem.popMatrix();
                     RenderSystem.matrixMode(5888);
                     RenderSystem.depthMask(true);
