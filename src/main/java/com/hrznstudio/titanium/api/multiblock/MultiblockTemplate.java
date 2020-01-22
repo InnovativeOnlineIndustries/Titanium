208e431726ac4e37f3bdd662629bf75498064f43
Lanse <simon_stalnabb@hotmail.com>
2020-01-22 22:00:52 +0100

    Added License Notifiers

--- src/main/java/com/hrznstudio/titanium/api/multiblock/MultiblockTemplate.java
+++ src/main/java/com/hrznstudio/titanium/api/multiblock/MultiblockTemplate.java
@@ -1,3 +1,10 @@
+/*
+ * This file is part of Titanium
+ * Copyright (C) 2020, Horizon Studio <contact@hrznstudio.com>.
+ *
+ * This code is licensed under GNU Lesser General Public License v3.0, the full license text can be found in LICENSE.txt
+ */
+
 package com.hrznstudio.titanium.api.multiblock;
 
 import com.google.common.collect.ImmutableList;
