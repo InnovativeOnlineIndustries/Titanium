65118a5d8d1335b8cb66d26e4a2f477d412b0445
Skylar <skylar.som@gmail.com>
2020-01-31 00:59:36 -0500

    Added shapeless enchant

---
+++ src/main/java/com/hrznstudio/titanium/recipe/shapelessenchant/ShapelessEnchantSerializer.java
@@ -0,0 +1,79 @@
+package com.hrznstudio.titanium.recipe.shapelessenchant;
+
+import com.google.common.collect.Maps;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.mojang.realmsclient.util.JsonUtils;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.crafting.ShapedRecipe;
+import net.minecraft.item.crafting.ShapelessRecipe;
+import net.minecraft.util.JSONUtils;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.apache.commons.lang3.tuple.Pair;
+
+import javax.annotation.Nonnull;
+import java.util.Map;
+
+public class ShapelessEnchantSerializer extends ShapelessRecipe.Serializer {
+    @Nonnull
+    public ShapelessRecipe read(@Nonnull ResourceLocation recipeId, JsonObject json) {
+        String s = JSONUtils.getString(json, "group", "");
+        NonNullList<Ingredient> ingredients = readIngredients(JSONUtils.getJsonArray(json, "ingredients"));
+        if (ingredients.isEmpty()) {
+            throw new JsonParseException("No ingredients for shapeless recipe");
+        } else if (ingredients.size() > 9) {
+            throw new JsonParseException("Too many ingredients for shapeless recipe the max is " + 9);
+        } else {
+            JsonObject jsonObject = JSONUtils.getJsonObject(json, "result");
+            ItemStack itemstack = ShapedRecipe.deserializeItem(jsonObject);
+            if (jsonObject.has("enchantments")) {
+                JsonElement enchantments = jsonObject.get("enchantments");
+                Map<Enchantment, Integer> enchantmentLevelMap = Maps.newHashMap();
+                if (enchantments.isJsonArray()) {
+                    for (JsonElement jsonElement: enchantments.getAsJsonArray()) {
+                        if (jsonElement.isJsonObject()) {
+                            Pair<Enchantment, Integer> enchantmentLevelPair = parseJson(jsonElement.getAsJsonObject());
+                            enchantmentLevelMap.put(enchantmentLevelPair.getKey(), enchantmentLevelPair.getValue());
+                        }
+                    }
+                } else if (enchantments.isJsonObject()) {
+                    Pair<Enchantment, Integer> enchantmentLevelPair = parseJson(enchantments.getAsJsonObject());
+                    enchantmentLevelMap.put(enchantmentLevelPair.getKey(), enchantmentLevelPair.getValue());
+                }
+                EnchantmentHelper.setEnchantments(enchantmentLevelMap, itemstack);
+            } else {
+                throw new JsonParseException("No String or Array found for enchantments");
+            }
+            return new ShapelessRecipe(recipeId, s, itemstack, ingredients);
+        }
+    }
+
+    private static Pair<Enchantment, Integer> parseJson(JsonObject jsonObject) {
+        String name = JSONUtils.getString(jsonObject, "name");
+        Enchantment enchantment = ForgeRegistries.ENCHANTMENTS.getValue(new ResourceLocation(name));
+        if (enchantment == null) {
+            throw new JsonParseException("Failed to find enchantment named: " + name);
+        }
+        return Pair.of(enchantment, JSONUtils.getInt(jsonObject, "level", 1));
+    }
+
+    private static NonNullList<Ingredient> readIngredients(JsonArray p_199568_0_) {
+        NonNullList<Ingredient> ingredients = NonNullList.create();
+
+        for(int i = 0; i < p_199568_0_.size(); ++i) {
+            Ingredient ingredient = Ingredient.deserialize(p_199568_0_.get(i));
+            if (!ingredient.hasNoMatchingItems()) {
+                ingredients.add(ingredient);
+            }
+        }
+
+        return ingredients;
+    }
+}
