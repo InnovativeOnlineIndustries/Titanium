a0595e9768a9deb4d4b8450b5a017d5bdf53e33f
Skylar Sommers <skylar.som@gmail.com>
2020-04-04 14:58:12 -0400

    Titanium recipes no longer error if test recipes are off (#44)
    * Titanium recipes no longer error if test recipes are off
    
    * License Format

---
+++ src/main/java/com/hrznstudio/titanium/recipe/condition/ContentExistsConditionSerializer.java
@@ -0,0 +1,39 @@
+/*
+ * This file is part of Titanium
+ * Copyright (C) 2020, Horizon Studio <contact@hrznstudio.com>.
+ *
+ * This code is licensed under GNU Lesser General Public License v3.0, the full license text can be found in LICENSE.txt
+ */
+
+package com.hrznstudio.titanium.recipe.condition;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import net.minecraft.util.JSONUtils;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.crafting.conditions.IConditionSerializer;
+import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.RegistryManager;
+
+public class ContentExistsConditionSerializer implements IConditionSerializer<ContentExistsCondition> {
+    @Override
+    public void write(JsonObject json, ContentExistsCondition value) {
+        json.addProperty("registry", value.getForgeRegistry().getRegistryName().toString());
+        json.addProperty("name", value.getContentName().toString());
+    }
+
+    @Override
+    public ContentExistsCondition read(JsonObject json) {
+        String registryName = JSONUtils.getString(json, "registry");
+        IForgeRegistry<?> forgeRegistry = RegistryManager.ACTIVE.getRegistry(new ResourceLocation(registryName));
+        if (forgeRegistry == null) {
+            throw new JsonParseException("Didn't Find Registry for registry: " + registryName);
+        }
+        return new ContentExistsCondition(forgeRegistry, new ResourceLocation(JSONUtils.getString(json, "name")));
+    }
+
+    @Override
+    public ResourceLocation getID() {
+        return ContentExistsCondition.NAME;
+    }
+}
