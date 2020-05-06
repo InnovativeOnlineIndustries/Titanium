c4a2486ce425384311d9481aa09ff3412710a4df
Lanse <simon_stalnabb@hotmail.com>
2020-05-06 17:29:36 +0200

    Added JsonProvider for the GenericSerializer system

--- src/main/java/com/hrznstudio/titanium/json/jsonprovider/IJsonProvider.java
+++ src/main/java/com/hrznstudio/titanium/json/jsonprovider/IJsonProvider.java
@@ -5,7 +5,7 @@
  * This code is licensed under GNU Lesser General Public License v3.0, the full license text can be found in LICENSE.txt
  */
 
-package com.hrznstudio.titanium.json;
+package com.hrznstudio.titanium.json.jsonprovider;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
