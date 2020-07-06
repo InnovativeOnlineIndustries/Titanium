62585dcce37ff4cfd30a2346900d4c0e6d1e3aaf
Buuz135 <buuz135@hotmail.com>
2020-07-06 17:50:08 +0200

    Changed IComponentHandler#add accept a collection of components

--- src/main/java/com/hrznstudio/titanium/component/IComponentHandler.java
+++ src/main/java/com/hrznstudio/titanium/component/IComponentHandler.java
@@ -9,7 +9,7 @@ package com.hrznstudio.titanium.component;
 
 public interface IComponentHandler<T> {
 
-    void add(T component);
+    void add(T... component);
 
     boolean accepts(Object component);
 
