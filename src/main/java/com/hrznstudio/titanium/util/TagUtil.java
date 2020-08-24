e7ae4da8432428d1b44c2c6d58efd7713051bf84
Buuz135 <buuz135@hotmail.com>
2020-08-24 20:47:23 +0200

    Fixed tags

--- src/main/java/com/hrznstudio/titanium/util/TagUtil.java
+++ src/main/java/com/hrznstudio/titanium/util/TagUtil.java
@@ -8,10 +8,11 @@
 package com.hrznstudio.titanium.util;
 
 import net.minecraft.block.Block;
+import net.minecraft.entity.EntityType;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.Item;
-import net.minecraft.tags.ITagCollection;
-import net.minecraft.tags.Tag;
+import net.minecraft.tags.*;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 
 import java.util.ArrayList;
@@ -52,4 +53,32 @@ public class TagUtil {
     public static <T> Collection<T> getAllEntries(Tag<T> tag) {
         return tag.getAllElements();
     } //getAllElements
+
+    public static <T> ITag<T> getOrCreateTag(ITagCollection<T> collection, ResourceLocation resourceLocation) {
+        if (collection.getRegisteredTags().contains(resourceLocation)) {
+            return collection.get(resourceLocation);
+        }
+        return collection.func_241834_b(resourceLocation);
+    }
+
+    public static ITag<Item> getItemTag(ResourceLocation resourceLocation) {
+        if (ItemTags.getCollection().getRegisteredTags().contains(resourceLocation)) {
+            return ItemTags.getCollection().get(resourceLocation);
+        }
+        return ItemTags.makeWrapperTag(resourceLocation.toString());
+    }
+
+    public static ITag<Block> getBlockTag(ResourceLocation resourceLocation) {
+        if (BlockTags.getCollection().getRegisteredTags().contains(resourceLocation)) {
+            return BlockTags.getCollection().get(resourceLocation);
+        }
+        return BlockTags.makeWrapperTag(resourceLocation.toString());
+    }
+
+    public static ITag<EntityType<?>> getEntityTypeTag(ResourceLocation resourceLocation) {
+        if (EntityTypeTags.getCollection().getRegisteredTags().contains(resourceLocation)) {
+            return EntityTypeTags.getCollection().get(resourceLocation);
+        }
+        return EntityTypeTags.func_232896_a_(resourceLocation.toString());
+    }
 }
