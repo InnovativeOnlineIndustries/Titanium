86febc0302f38f2ad98e1bd656a319acba00091a
Buuz135 <buuz135@hotmail.com>
2021-02-12 20:14:29 +0100

    Added Lockable Slot Bundle

---
+++ src/main/java/com/hrznstudio/titanium/client/screen/addon/LockableOverlayAddon.java
@@ -0,0 +1,54 @@
+package com.hrznstudio.titanium.client.screen.addon;
+
+import com.hrznstudio.titanium.api.client.AssetTypes;
+import com.hrznstudio.titanium.api.client.IAsset;
+import com.hrznstudio.titanium.client.screen.asset.IAssetProvider;
+import com.hrznstudio.titanium.component.inventory.SidedInventoryComponent;
+import com.hrznstudio.titanium.util.AssetUtil;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import net.minecraft.client.gui.screen.Screen;
+
+import java.awt.*;
+
+public class LockableOverlayAddon extends BasicScreenAddon{
+
+    private SidedInventoryComponent component;
+    private int xSize;
+    private int ySize;
+
+    public LockableOverlayAddon(SidedInventoryComponent inventoryComponent, int x, int y) {
+        super(x, y);
+        this.component = inventoryComponent;
+        this.xSize = 0;
+        this.ySize = 0;
+    }
+
+    @Override
+    public void drawBackgroundLayer(MatrixStack stack, Screen screen, IAssetProvider provider, int guiX, int guiY, int mouseX, int mouseY, float partialTicks) {
+        this.xSize = provider.getAsset(AssetTypes.BUTTON_SIDENESS_MANAGER).getArea().width +2;
+        this.ySize = provider.getAsset(AssetTypes.BUTTON_SIDENESS_MANAGER).getArea().height;
+    }
+
+    @Override
+    public void drawForegroundLayer(MatrixStack stack, Screen screen, IAssetProvider provider, int guiX, int guiY, int mouseX, int mouseY) {
+        if (isInside(screen, mouseX - guiX, mouseY - guiY)) {
+            int extra = 4;
+            IAsset asset = provider.getAsset(AssetTypes.BUTTON_SIDENESS_MANAGER);
+            Rectangle area = component.getRectangle(asset);
+            AssetUtil.drawHorizontalLine(stack, area.x, area.x + area.width + extra, area.y, component.getColor());
+            AssetUtil.drawHorizontalLine(stack, area.x, area.x + area.width + extra, area.y + area.height + extra, component.getColor());
+            AssetUtil.drawVerticalLine(stack, area.x, area.y, area.y + area.height + extra, component.getColor());
+            AssetUtil.drawVerticalLine(stack, area.x + area.width + extra, area.y , area.y + area.height + extra, component.getColor());
+        }
+    }
+
+    @Override
+    public int getXSize() {
+        return this.xSize;
+    }
+
+    @Override
+    public int getYSize() {
+        return this.ySize;
+    }
+}
---
+++ src/main/java/com/hrznstudio/titanium/component/bundle/LockableInventoryBundle.java
@@ -0,0 +1,133 @@
+package com.hrznstudio.titanium.component.bundle;
+
+import com.hrznstudio.titanium.api.IFactory;
+import com.hrznstudio.titanium.api.client.AssetTypes;
+import com.hrznstudio.titanium.api.client.IScreenAddon;
+import com.hrznstudio.titanium.block.tile.BasicTile;
+import com.hrznstudio.titanium.client.screen.addon.LockableOverlayAddon;
+import com.hrznstudio.titanium.client.screen.addon.StateButtonAddon;
+import com.hrznstudio.titanium.client.screen.addon.StateButtonInfo;
+import com.hrznstudio.titanium.component.IComponentBundle;
+import com.hrznstudio.titanium.component.IComponentHandler;
+import com.hrznstudio.titanium.component.IComponentHarness;
+import com.hrznstudio.titanium.component.button.ButtonComponent;
+import com.hrznstudio.titanium.component.inventory.InventoryComponent;
+import com.hrznstudio.titanium.component.inventory.SidedInventoryComponent;
+import com.hrznstudio.titanium.container.addon.IContainerAddon;
+import com.hrznstudio.titanium.util.LangUtil;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.common.util.INBTSerializable;
+
+import javax.annotation.Nonnull;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.BiPredicate;
+
+public class LockableInventoryBundle<T extends BasicTile & IComponentHarness> implements IComponentBundle, INBTSerializable<CompoundNBT> {
+
+    private InventoryComponent<T> inventory;
+    private BiPredicate<ItemStack, Integer> cachedFilter;
+    private ButtonComponent buttonAddon;
+    private T componentHarness;
+    private ItemStack[] filter;
+    private int lockPosX;
+    private int lockPosY;
+    private boolean isLocked;
+
+    public LockableInventoryBundle(T componentHarness, InventoryComponent<T> inventory, int lockPosX, int lockPosY, boolean isLocked) {
+        this.componentHarness = componentHarness;
+        this.inventory = inventory;
+        this.cachedFilter = inventory.getInsertPredicate();
+        this.filter = new ItemStack[this.inventory.getSlots()];
+        Arrays.fill(this.filter, ItemStack.EMPTY);
+        this.lockPosX = lockPosX;
+        this.lockPosY = lockPosY;
+        this.isLocked = isLocked;
+        this.buttonAddon = new ButtonComponent(lockPosX, lockPosY, 14,14){
+            @Override
+            public List<IFactory<? extends IScreenAddon>> getScreenAddons() {
+                return Collections.singletonList(() -> new StateButtonAddon(buttonAddon,
+                    new StateButtonInfo(0, AssetTypes.BUTTON_UNLOCKED, TextFormatting.GOLD + LangUtil.getString("tooltip.titanium.locks") +  TextFormatting.WHITE +  " " + LangUtil.getString("tooltip.titanium.facing_handler." + inventory.getName().toLowerCase())),
+                    new StateButtonInfo(1, AssetTypes.BUTTON_LOCKED, TextFormatting.GOLD + LangUtil.getString("tooltip.titanium.unlocks") + TextFormatting.WHITE + " " + LangUtil.getString("tooltip.titanium.facing_handler." + inventory.getName().toLowerCase()))) {
+                    @Override
+                    public int getState() {
+                        return LockableInventoryBundle.this.isLocked ? 1 : 0;
+                    }
+                });
+            }
+        }.setPredicate((playerEntity, compoundNBT) -> {
+                this.isLocked = !this.isLocked;
+            for (int i = 0; i < this.inventory.getSlots(); i++) {
+                this.filter[i] = this.inventory.getStackInSlot(i).copy();
+            }
+                updateFilter();
+                this.componentHarness.syncObject(this);
+            });
+    }
+
+    @Nonnull
+    @Override
+    public List<IFactory<? extends IScreenAddon>> getScreenAddons() {
+        return inventory instanceof SidedInventoryComponent ? Collections.singletonList(() -> new LockableOverlayAddon((SidedInventoryComponent) inventory, this.lockPosX, this.lockPosY)) : Collections.emptyList();
+    }
+
+    @Override
+    public void accept(IComponentHandler... handler) {
+        for (IComponentHandler iComponentHandler : handler) {
+            iComponentHandler.add(this.inventory, this.buttonAddon);
+        }
+    }
+
+    @Nonnull
+    @Override
+    public List<IFactory<? extends IContainerAddon>> getContainerAddons() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public CompoundNBT serializeNBT() {
+        CompoundNBT compoundNBT = new CompoundNBT();
+        compoundNBT.put("Inventory", this.inventory.serializeNBT());
+        compoundNBT.putBoolean("Locked", this.isLocked);
+        ListNBT nbt = new ListNBT();
+        for (ItemStack stack : this.filter) {
+            nbt.add(stack.serializeNBT());
+        }
+        compoundNBT.put("Filter", nbt);
+        return compoundNBT;
+    }
+
+    @Override
+    public void deserializeNBT(CompoundNBT nbt) {
+        this.inventory.deserializeNBT(nbt.getCompound("Inventory"));
+        this.isLocked = nbt.getBoolean("Locked");
+        ListNBT list = (ListNBT) nbt.get("Filter");
+        this.filter = new ItemStack[list.size()];
+        Arrays.fill(this.filter, ItemStack.EMPTY);
+        for (int i = 0; i < list.size(); i++) {
+            this.filter[i] = ItemStack.read(list.getCompound(i));
+        }
+        updateFilter();
+    }
+
+    private void updateFilter(){
+        if (isLocked){
+            this.inventory.setInputFilter((stack, integer) -> !this.filter[integer].isEmpty() && this.filter[integer].isItemEqual(stack));
+        } else {
+            Arrays.fill(this.filter, ItemStack.EMPTY);
+            this.inventory.setInputFilter(this.cachedFilter);
+        }
+        for (int i = 0; i < this.filter.length; i++) {
+            this.inventory.setSlotToItemStackRender(i, this.filter[i]);
+        }
+    }
+
+    public InventoryComponent<T> getInventory() {
+        return inventory;
+    }
+
+}
