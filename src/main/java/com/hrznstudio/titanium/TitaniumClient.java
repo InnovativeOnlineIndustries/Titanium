a05029129cec963ff09abc5b1707b2031ba8ff37
Buuz135 <buuz135@hotmail.com>
2021-05-09 18:07:50 +0200

    Improved voxelshapes ray trace and rendering

--- src/main/java/com/hrznstudio/titanium/TitaniumClient.java
+++ src/main/java/com/hrznstudio/titanium/TitaniumClient.java
@@ -15,11 +15,11 @@ import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.entity.PlayerRenderer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraft.util.math.vector.Vector3f;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -42,8 +42,8 @@ public class TitaniumClient {
             if (og.getBlock() instanceof BasicBlock && ((BasicBlock) og.getBlock()).hasIndividualRenderVoxelShape()) {
                 VoxelShape shape = RayTraceUtils.rayTraceVoxelShape(traceResult, Minecraft.getInstance().world, Minecraft.getInstance().player, 32, event.getPartialTicks());
                 BlockPos blockpos = event.getTarget().getPos();
-                if (shape != null) {
-                    event.setCanceled(true);
+                event.setCanceled(true);
+                if (shape != null && !shape.isEmpty()) {
                     MatrixStack stack = new MatrixStack();
                     stack.push();
                     ActiveRenderInfo info = event.getInfo();
@@ -53,11 +53,19 @@ public class TitaniumClient {
                     double d1 = info.getProjectedView().getY();
                     double d2 = info.getProjectedView().getZ();
                     IVertexBuilder builder = Minecraft.getInstance().getRenderTypeBuffers().getOutlineBufferSource().getBuffer(RenderType.LINES);
-                    WorldRenderer.drawBoundingBox(stack, builder, shape.getBoundingBox().offset(blockpos.getX() - d0,
-                            blockpos.getY() - d1, blockpos.getZ() - d2), 0, 0, 0, 0.5F);
+                    drawShape(stack, builder, shape, blockpos.getX() - d0,
+                        blockpos.getY() - d1, blockpos.getZ() - d2,0, 0, 0, 0.5F);
                     stack.pop();
                 }
             }
         }
+    }
+
+    private static void drawShape(MatrixStack matrixStackIn, IVertexBuilder bufferIn, VoxelShape shapeIn, double xIn, double yIn, double zIn, float red, float green, float blue, float alpha) {
+        Matrix4f matrix4f = matrixStackIn.getLast().getMatrix();
+        shapeIn.forEachEdge((p_230013_12_, p_230013_14_, p_230013_16_, p_230013_18_, p_230013_20_, p_230013_22_) -> {
+            bufferIn.pos(matrix4f, (float)(p_230013_12_ + xIn), (float)(p_230013_14_ + yIn), (float)(p_230013_16_ + zIn)).color(red, green, blue, alpha).endVertex();
+            bufferIn.pos(matrix4f, (float)(p_230013_18_ + xIn), (float)(p_230013_20_ + yIn), (float)(p_230013_22_ + zIn)).color(red, green, blue, alpha).endVertex();
+        });
     }
 }
--- src/main/java/com/hrznstudio/titanium/util/RayTraceUtils.java
+++ src/main/java/com/hrznstudio/titanium/util/RayTraceUtils.java
@@ -14,6 +14,7 @@ import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceContext;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 
@@ -37,17 +38,19 @@ public class RayTraceUtils {
             Vector3d vec3d1 = living.getLook(partialTicks);
             Vector3d vec3d2 = vec3d.add(vec3d1.x * blockReachDistance, vec3d1.y * blockReachDistance, vec3d1.z * blockReachDistance);
             List<VoxelShape> voxelShapes = ((BasicBlock) og.getBlock()).getBoundingBoxes(og, world, ((BlockRayTraceResult) original).getPos());
-            VoxelShape closest = voxelShapes.get(0);
-            double distance = Double.MAX_VALUE;
-            for (VoxelShape voxelShape : voxelShapes) {
-                BlockRayTraceResult result = voxelShape.rayTrace(vec3d, vec3d2, ((BlockRayTraceResult) original).getPos());
-                if (result != null && vec3d.distanceTo(result.getHitVec()) < distance) {
-                    closest = voxelShape;
-                    distance = vec3d.distanceTo(result.getHitVec());
+            if (voxelShapes.size() > 0){
+                VoxelShape closest = VoxelShapes.empty();
+                double distance = Double.MAX_VALUE;
+                for (VoxelShape voxelShape : voxelShapes) {
+                    BlockRayTraceResult result = voxelShape.rayTrace(vec3d, vec3d2, ((BlockRayTraceResult) original).getPos());
+                    if (result != null && vec3d.distanceTo(result.getHitVec()) < distance) {
+                        closest = voxelShape;
+                        distance = vec3d.distanceTo(result.getHitVec());
+                    }
                 }
+                return closest;
             }
-            return closest;
         }
-        return null;
+        return VoxelShapes.empty();
     }
 }
