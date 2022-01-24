429d63f9348d42cd3c9a1511d68f43d2fc83085a
Buuz135 <buuz135@hotmail.com>
2022-01-24 19:21:07 +0100

    Updated license

--- src/main/java/com/hrznstudio/titanium/container/IDisableableContainer.java
+++ src/main/java/com/hrznstudio/titanium/container/IDisableableContainer.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of Titanium
- * Copyright (C) 2021, Horizon Studio <contact@hrznstudio.com>.
+ * Copyright (C) 2022, Horizon Studio <contact@hrznstudio.com>.
  *
  * This code is licensed under GNU Lesser General Public License v3.0, the full license text can be found in LICENSE.txt
  */
