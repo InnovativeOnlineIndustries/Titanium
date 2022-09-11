71adc8050f75919d3f7dc03eeb48bf27aca307e5
Buuz135 <buuz135@hotmail.com>
2022-09-11 22:28:21 +0200

    License format

--- src/main/java/com/hrznstudio/titanium/block_network/graph/NetworkGraphScannerRequest.java
+++ src/main/java/com/hrznstudio/titanium/block_network/graph/NetworkGraphScannerRequest.java
@@ -1,3 +1,10 @@
+/*
+ * This file is part of Titanium
+ * Copyright (C) 2022, Horizon Studio <contact@hrznstudio.com>.
+ *
+ * This code is licensed under GNU Lesser General Public License v3.0, the full license text can be found in LICENSE.txt
+ */
+
 package com.hrznstudio.titanium.block_network.graph;
 
 import net.minecraft.core.BlockPos;
